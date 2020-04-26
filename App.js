8cf90dbc9468eb75577f8c5f489fca7572c8a9ee
Jordi <ofineo@gmail.com>
2020-04-26 14:35:47 +0100

    added font to project and loaded Async

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Fonts from 'expo-font';
+import { AppLoading } from 'expo;'
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,11 +26,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
