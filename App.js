20d7cfacd9e3334d2c2746229d243d9117ddc6c5
Jordi <ofineo@gmail.com>
2020-04-26 16:09:51 +0100

    added navigation with react-navigation

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Fonts from 'expo-font';
-import { AppLoading } from 'expo;'
+import { AppLoading } from 'expo';
+import MealsNavigator from './navigation/MealsNavigator';
+
 
 const fetchFonts = () => {
   Font.loadAsync({
@@ -20,9 +22,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator/>
   );
 }
 
