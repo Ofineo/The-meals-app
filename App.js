b250e1108bd75e26bef3e7e7926e1389ba95a6a4
Jordi <ofineo@gmail.com>
2020-04-27 09:56:39 +0100

    refactor CategoriesMealsScreen to show dummy data for recipes

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import * as Fonts from 'expo-font';
 import { AppLoading } from 'expo';
 import MealsNavigator from './navigation/MealsNavigator';
+import { enableScreens } from 'react-native-screens';
 
+enableScreens();
 
 const fetchFonts = () => {
-  Font.loadAsync({
+  Fonts.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   })
--- screens/CategoriesMealsScreen.js
+++ screens/CategoriesMealsScreen.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { View,Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-const CategoriesMealsScreen = props=>{
-    return(
-        <View style={styles.screen}>
-            <Text>CategoriesMealsScreen Screen</Text>
-            <Button title="Go to Meal Detail" onPress={()=>{ props.navigation.navigate({routeName:'Mealdetail'})}}/>
-        </View>
-
+const CategoriesMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
     );
+  };
+  const catId = props.navigation.getParam("categoryId");
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  return (
+    <View style={styles.screen}>
+      <FlatList data={displayedMeals} renderItem={renderMealItem} />
+    </View>
+  );
+};
+
+CategoriesMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCat = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    title: selectedCat.title,
+  };
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default CategoriesMealsScreen;
